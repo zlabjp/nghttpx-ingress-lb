6ff7516416cc5f51b37766c9948eb8a653c7a822
Tatsuhiro Tsujikawa <ttsujika@zlab.co.jp>
2017-01-06 12:08:02 +0900

    Add test for FixupPortBackendConfig

---
+++ pkg/nghttpx/utils_test.go
@@ -0,0 +1,48 @@
+package nghttpx
+
+import (
+	"testing"
+)
+
+// TestFixupPortBackendConfig validates fixupPortBackendConfig corrects invalid input to the correct default value.
+func TestFixupPortBackendConfig(t *testing.T) {
+	tests := []struct {
+		in  PortBackendConfig
+		out PortBackendConfig
+	}{
+		{
+			in: PortBackendConfig{
+				Proto:    "foo",
+				Affinity: "bar",
+			},
+			out: PortBackendConfig{
+				Proto:    ProtocolH1,
+				Affinity: AffinityNone,
+			},
+		},
+		{
+			// Empty Proto and Affinity should be replaced with default values.
+			out: PortBackendConfig{
+				Proto:    ProtocolH1,
+				Affinity: AffinityNone,
+			},
+		},
+		{
+			// Correct input must be left unchanged.
+			in: PortBackendConfig{
+				Proto:    ProtocolH2,
+				Affinity: AffinityIP,
+			},
+			out: PortBackendConfig{
+				Proto:    ProtocolH2,
+				Affinity: AffinityIP,
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		if got, want := FixupPortBackendConfig(tt.in, "svc", "port"), tt.out; got != want {
+			t.Errorf("#%v: fixupPortBackendConfig(%+v) = %+v, want %+v", i, tt.in, got, want)
+		}
+	}
+}
