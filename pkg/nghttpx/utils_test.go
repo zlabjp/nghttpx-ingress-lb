69a5f8750c2dcba6141dbbc8d2c00c9d704402cc
Tatsuhiro Tsujikawa <ttsujika@zlab.co.jp>
2017-02-24 16:01:06 +0900

    Add missing copyright notice

--- pkg/nghttpx/utils_test.go
+++ pkg/nghttpx/utils_test.go
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2016, Z Lab Corporation. All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 package nghttpx
 
 import (
